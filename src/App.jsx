0bb16c491bdaae174e3d7d3f27028a0d8855e44b
Pradhan321 <avinashpradhan717@gmail.com>
2025-08-13 13:48:01 +0530

    Setting up Ai-Resume-Builder

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+   <>
+   subscribe to my channel
+  <Button>Subscribe</Button>
+    </>
+  )
+}
+
+export default App
